ae49d05c5f46064588061911daa306151abf56f1
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-04-30 00:05:19 +0600

    Tailwind css And Google Font Added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            <h1>This is Header Area</h1>
+            <h1 className="text-red-600">This is Header Area</h1>
         </div>
     );
 };
