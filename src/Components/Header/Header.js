bfdf7d3b705a9ad48bc6c8a95210aca1eef9680f
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-05-01 00:29:48 +0600

    Header Area Desktop version done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,30 +6,22 @@ import search from '../../Images/Search.png';
 
 const Header = () => {
     return (
-        <nav className="relative w-full flex flex-wrap items-center justify-between py-4">
-            <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
-                <div className="flex-grow items-center">
-                    <a className="flex items-center mt-2 lg:mt-0 mr-1" href="#test">
-                        <p>Starbucks</p>
-                        <img src={arrow} alt="Down Arrow" />
-                        <div className="relative mr-3">
-                            <div className="flex absolute inset-y-0 left-0 items-center pl-3">
-                                <img src={search} alt="Search Icon" />
-                            </div>
-                            <input type="text" className="block p-2 pl-10 w-full rounded-lg border sm:text-sm" placeholder="Search..." />
-                        </div>
-                    </a>
-                </div>
-                <div className="flex items-center relative">
-                    <div className="relative">
-                        <img src={bell} alt="Bell Icon" />
+        <nav className="flex items-center justify-between pl-[26px] pr-[18px] py-2">
+            <div className="flex items-center">
+                <p className="text-[#3E3F48] text-[14px] font-[500]">Starbucks</p>
+                <img className="ml-[7px] mr-[7px]" src={arrow} alt="Down Arrow" />
+                <div className="relative lg:ml-[128px] md:ml-[10px] hidden sm:block">
+                    <div className="flex absolute inset-y-0 left-0 items-center pl-3">
+                        <img className=' w-[16.67px] h-[16.67px]' src={search} alt="Search Icon" />
                     </div>
-                    <div className="relative flex justify-center items-center">
-                        <img className="flex items-center rounded-full" src={user} alt="User" />
-                        <p>John Doe</p>
-                        <img src={arrow} alt="Down Arrow" />
-                    </div>
+                    <input type="text" className="w-[392px] h-10 pl-10 bg-[#F2F2F3] rounded-[8px]" placeholder="Search..." />
                 </div>
+            </div>
+            <div className="flex items-center justify-end relative w-full">
+                <img src={bell} alt="Bell Icon" />
+                <img className="flex items-center rounded-full mr-[16px] ml-[19px]" src={user} alt="User" />
+                <p className="text-[14px] font-[500]">John Doe</p>
+                <img className="ml-[11px]" src={arrow} alt="Down Arrow" />
             </div>
         </nav>
     );
