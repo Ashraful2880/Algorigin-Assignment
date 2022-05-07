b4f7c6ae0a200b165fce455dc7f96fb55f522cb5
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-05-07 16:41:57 +0600

    All route Check done

--- src/Components/Dashboard/Documents/Documents.js
+++ src/Components/Dashboard/Documents/Documents.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Documents = () => {
     return (
-        <div className="bg-[#F2F2F3] h-screen">
+        <div className="bg-[#F2F2F3] h-[85vh]">
             <h1 className="text-left text-[#3E3F48] text-[20px] font-[700] h-[49px] border-b border-b-[#E5E5E6] pl-[24px] pt-[4px]">Documents</h1>
         </div>
     );
--- src/Components/Dashboard/Inventory/Inventory.js
+++ src/Components/Dashboard/Inventory/Inventory.js
@@ -11,7 +11,7 @@ const Inventory = () => {
             .then(data => setItems(data))
     }, [])
     return (
-        <div className="bg-[#F2F2F3] h-screen">
+        <div className="bg-[#F2F2F3] h-fit">
             <h1 className="text-left text-[#3E3F48] text-[20px] font-[700] h-[49px] border-b border-b-[#E5E5E6] pl-[24px] pt-[4px]">Inventory</h1>
             <div className="bg-white m-[24px] rounded-[4px] shadow-md">
                 <div className="w-1/4 ml-[16px] py-[24px]">
