c495c77d76285623b529657e24e496990cc2599b
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-05-07 16:36:56 +0600

    Site Title & Mobile menu Margin Ahange

--- src/Components/Dashboard/DashboardHome/DashboardHome.js
+++ src/Components/Dashboard/DashboardHome/DashboardHome.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DashboardHome = () => {
     return (
-        <div className="bg-[#F2F2F3] h-screen">
+        <div className="bg-[#F2F2F3] h-[85vh]">
             <h1 className="text-left text-[#3E3F48] text-[20px] font-[700] h-[49px] border-b border-b-[#E5E5E6] pl-[24px] pt-[4px]">Dashboard Home</h1>
         </div>
     );
